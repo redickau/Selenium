e117d68c4b11d34519668ebbbfb903d4216902ad
redickau <aredick12@gmail.com>
2014-02-06 15:03:13 -0600

    bleh

--- misc_projects/nodejs_stuff/SimpleServer/Makefile
+++
@@ -1,2 +0,0 @@
-clean:
-	rm -rf *.pl~ *~ *.pl# *#
--- misc_projects/nodejs_stuff/SimpleServer/index.js
+++
@@ -1,7 +0,0 @@
-// Require the module startServer to make use of its functions
-var myServer = require("./startServer");
-
-// Start a new http server
-myServer.startServer();
-
-	
